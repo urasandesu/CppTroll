88917612735d8565385fac0a6856d6ec13d95f76
Akira Sugiura <urasandesu@gmail.com>
2011-04-26 21:37:06 +0900

    Investigate for Boost. 01: Compile Time Assertion.

---
+++ BoostInvestigation01/BoostInvestigation01.cpp
@@ -0,0 +1,55 @@
+// BoostInvestigation01.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <boost/static_assert.hpp>
+#include <boost/concept_check.hpp>
+#include <boost/concept/requires.hpp>
+
+namespace std
+{
+    struct any
+    {
+    };
+
+    template<>
+	class vector<any>
+    {
+    public:
+        template<class T>
+        vector(vector<T>& other) { }
+
+        template<class T>
+        vector<any>& operator=(vector<T>& other) { return *this; } 
+    };
+}
+
+template<class T>
+BOOST_CONCEPT_REQUIRES(
+                       ((boost::Convertible<T, std::vector<std::any>>)),
+                       (void)
+                       ) Hoge(T& t)
+{
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    using namespace std;
+    using namespace boost;
+
+    vector<int> v1;
+    Hoge(v1);
+    vector<double> v2;
+    Hoge(v2);
+    int i = 0;
+    //Hoge(i);      // コンパイルエラー
+    struct Piyo { } piyo;
+    //Hoge(piyo);   // コンパイルエラー
+    BOOST_CONCEPT_ASSERT((Convertible<vector<int>, vector<any>>));
+    BOOST_STATIC_ASSERT((is_convertible<vector<int>, vector<any>>::value));
+    BOOST_STATIC_ASSERT((!is_convertible<vector<int>, vector<double>>::value));
+
+    return 0;
+}
+
