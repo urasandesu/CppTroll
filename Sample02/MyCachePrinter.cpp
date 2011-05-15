1f1249ff0ef73bed550be9e4be48a2cef0adce72
Akira Sugiura <urasandesu@gmail.com>
2011-05-16 08:01:39 +0900

    Investigate for Boost and ATL. 02: Fix a bug that ignores HRESULT of CComVariant::ChangeType.

--- Sample02/MyCachePrinter.cpp
+++ Sample02/MyCachePrinter.cpp
@@ -6,6 +6,74 @@
 
 // CMyCachePrinter
 
+HRESULT ToString(const CComVariant& value, BSTR* pStr)
+{
+    HRESULT hr;
+
+    CComVariant _value = value;
+	switch (_value.vt)
+	{
+	case VT_UNKNOWN:
+		{
+            CComBSTR str(L"IUnknown");
+            hr = str.CopyTo(pStr);
+            if (FAILED(hr))
+            {
+                return hr;
+            }
+            break;
+		}
+	case VT_DISPATCH:
+		{
+            CComBSTR str(L"IDispatch");
+            hr = str.CopyTo(pStr);
+            if (FAILED(hr))
+            {
+                return hr;
+            }
+            break;
+		}
+    case VT_BSTR:
+        {
+            hr = _value.CopyTo(pStr);
+            if (FAILED(hr))
+            {
+                return hr;
+            }
+            break;
+        }
+	case VT_UI1:
+	case VT_I1:
+	case VT_I2:
+	case VT_UI2:
+	case VT_BOOL:
+	case VT_I4:
+	case VT_UI4:
+	case VT_R4:
+	case VT_INT:
+	case VT_UINT:
+	case VT_ERROR:
+	case VT_I8:
+	case VT_UI8:
+	case VT_R8:
+	case VT_CY:
+	case VT_DATE:
+        {
+            _value.ChangeType(VT_BSTR);
+            hr = _value.CopyTo(pStr);
+            if (FAILED(hr))
+            {
+                return hr;
+            }
+            break;
+        }
+	default:
+		return E_INVALIDARG;
+	}   // switch (_value.vt)
+
+    return S_OK;
+}
+
 STDMETHODIMP CMyCachePrinter::Print(void)
 {
     using namespace std;
@@ -58,13 +126,14 @@ STDMETHODIMP CMyCachePrinter::Print(void)
             return hr;
         }
 
-        second.ChangeType(VT_BSTR);
+        BSTR str;
+        hr = ToString(second, &str);
         if (FAILED(hr))
         {
             return hr;
         }
 
-        cout << format("MyCache[%|1|]:  %|2|") % CW2A(first) % CW2A(second.bstrVal) << endl;
+        cout << format("MyCache[%|1|]:  %|2|") % CW2A(first) % CW2A(str) << endl;
     }
 
     return S_OK;
