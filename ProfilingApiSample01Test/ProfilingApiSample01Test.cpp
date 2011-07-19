edc4f38b9df32d12f5817f0f482b1754cbb135f3
Akira Sugiura <urasandesu@gmail.com>
2011-07-19 18:42:35 +0900

    Investigate for Unmanaged Profiling API. 01: Add the project to plan to design the I/F for Unmanaged Profiling API and add the project to test it in the native environment.

---
+++ ProfilingApiSample01Test/ProfilingApiSample01Test.cpp
@@ -0,0 +1,19 @@
+// ProfilingApiSample01Test.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#define BOOST_TEST_NO_LIB
+#include <boost/test/unit_test.hpp>
+
+namespace {
+
+    BOOST_AUTO_TEST_SUITE(ProfilingApiSample01TestSuite)
+
+    BOOST_AUTO_TEST_CASE(ProfilingApiSample01Test)
+    {
+        BOOST_REQUIRE(true);
+    }
+
+    BOOST_AUTO_TEST_SUITE_END()
+}
