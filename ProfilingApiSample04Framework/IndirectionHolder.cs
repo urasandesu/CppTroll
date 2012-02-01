44a344694cf7ccc385b858a4c9a8d5dbba0e171b
Akira Sugiura <urasandesu@gmail.com>
2012-02-02 08:01:23 +0900

    Unmanaged Profiling API Sample 04: Add Result Of AppDomain Investigation
    Add CrossDomainDictionary that can access to theirs data from other AppDomain. When I create this class, I used Mr. Einar's "Cross AppDomain Singleton" as a reference(see also he's blog, http://www.dolittle.com/blogs/einar/archive/2007/05/18/cross-appdomain-singleton.aspx).
    Add LooseDomain that can instantiate any classes from other AppDomain: they don't take to extend MarshalByRefObject or to set SerializableAttribute!!. This trick is used of the behavior that CLR doesn't check the type matching between AppDomains if there isn't a opcode castclass.

---
+++ ProfilingApiSample04Framework/IndirectionHolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProfilingApiSample04Framework
+{
+    public class IndirectionHolder<TDelegate>
+    {
+        IndirectionHolder() { }
+        static IndirectionHolder<TDelegate> ms_instance = new IndirectionHolder<TDelegate>();
+        public static IndirectionHolder<TDelegate> Instance { get { return ms_instance; } }
+
+        public bool TryGet(IndirectionInfo2 info, out TDelegate method)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct IndirectionInfo2
+    {
+        public string AssemblyName { get; set; }
+        public string TypeFullName { get; set; }
+        public string MethodName { get; set; }
+    }
+}
---
+++ ProfilingApiSample04Framework/LooseDomain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+using InstanceGetters = ProfilingApiSample04Framework.CrossDomainDictionary<System.Type, System.IntPtr>;
+
+namespace ProfilingApiSample04Framework
+{
+    public static class LooseDomain
+    {
+        public static void Register<T>(Func<T> instanceGetter) where T : class
+        {
+            LooseDomain<T>.Register(instanceGetter);
+        }
+
+        public static void Unload<T>() where T : class
+        {
+            LooseDomain<T>.Unload();
+        }
+
+        public static T Get<T>() where T : class
+        {
+            return LooseDomain<T>.Instance;
+        }
+
+        public static bool TryGet<T>(out T instance) where T : class
+        {
+            instance = LooseDomain<T>.InstanceOrDefault;
+            return instance != null;
+        }
+    }
+
+    public sealed class LooseDomain<T> where T : class
+    {
+        static readonly object ms_lockObj = new object();
+        static T ms_instance = null;
+        static bool ms_ready = false;
+
+        LooseDomain() { }
+
+        public static void Register(Func<T> instanceGetter)
+        {
+            Register(instanceGetter.Method);
+        }
+
+        public static void Register(MethodInfo method)
+        {
+            if (!method.IsStatic)
+                throw new ArgumentException("The parameter must be the reference of a " +
+                                            "static method.");
+
+            var funcPtr = method.MethodHandle.GetFunctionPointer();
+            InstanceGetters.Instance.AddIfNotExist(typeof(T), funcPtr);
+        }
+
+        static T GetInstance()
+        {
+            var funcPtr = InstanceGetters.Instance.GetIfExist(typeof(T));
+            if (funcPtr == default(IntPtr))
+                throw new InvalidOperationException("The instance getter of T has not been " +
+                            "registered yet. Please call the method Register and give a " +
+                            "instance getter to it.");
+
+            return GetInstanceCore(funcPtr);
+        }
+
+        static bool TryGetInstance(out T instance)
+        {
+            var funcPtr = InstanceGetters.Instance.GetIfExist(typeof(T));
+            if (funcPtr == default(IntPtr))
+            {
+                instance = null;
+                return false;
+            }
+            else
+            {
+                instance = GetInstanceCore(funcPtr);
+                return true;
+            }
+        }
+
+        static T GetInstanceCore(IntPtr funcPtr)
+        {
+            var instanceGetter = new DynamicMethod("Instantiator", typeof(T), null, typeof(T).Module);
+            var gen = instanceGetter.GetILGenerator();
+            if (IntPtr.Size == 4)
+            {
+                gen.Emit(OpCodes.Ldc_I4, funcPtr.ToInt32());
+            }
+            else if (IntPtr.Size == 8)
+            {
+                gen.Emit(OpCodes.Ldc_I8, funcPtr.ToInt64());
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+            gen.EmitCalli(OpCodes.Calli, CallingConventions.Standard, typeof(T), null, null);
+            gen.Emit(OpCodes.Ret);
+            return ((Func<T>)instanceGetter.CreateDelegate(typeof(Func<T>)))();
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (!ms_ready)
+                {
+                    lock (ms_lockObj)
+                    {
+                        if (!ms_ready)
+                        {
+                            ms_instance = GetInstance();
+                            Thread.MemoryBarrier();
+                            ms_ready = true;
+                        }
+                    }
+                }
+                // !! Type is not checked by CLR if a opcode castclass does not exist !!
+                return ms_instance;
+            }
+        }
+
+        public static T InstanceOrDefault
+        {
+            get
+            {
+                if (!ms_ready)
+                {
+                    lock (ms_lockObj)
+                    {
+                        if (!ms_ready)
+                        {
+                            var instance = default(T);
+                            if (TryGetInstance(out instance))
+                            {
+                                ms_instance = instance;
+                                Thread.MemoryBarrier();
+                                ms_ready = true;
+                            }
+                        }
+                    }
+                }
+                return ms_instance;
+            }
+        }
+
+        public static void Unload()
+        {
+            if (ms_ready)
+            {
+                lock (ms_lockObj)
+                {
+                    if (ms_ready)
+                    {
+                        InstanceGetters.Unload();
+                        ms_instance = null;
+                        Thread.MemoryBarrier();
+                        ms_ready = false;
+                    }
+                }
+            }
+        }
+    }
+}
